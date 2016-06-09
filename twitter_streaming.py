c80dac1bee2c089a8c34561b9920bcb697689ba9
Ubuntu <ubuntu@ip-172-31-2-104.us-west-2.compute.internal>
2016-06-09 00:56:33 +0000

    first commit

---
+++ twitter_streaming.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import time
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from kafka.producer import KafkaProducer
+
+
+ACCESS_TOKEN = '2905352843-Sm2UEyP0MmNkAU6ZMkhHcJDRrtxLN37lo6hERRB'
+ACCESS_SECRET = 'zLgNeRIMHJu5y013D4DyPgAkQXq3urhfC64OznWBgGlZx'
+CONSUMER_KEY = 'hBb9XKsTPAUV7QNx8b7h1E71M'
+CONSUMER_SECRET = '0mqPbVbEmRIVZMqJGzaJZlejnAffcVZa3paC66qBvdaJKMkezt'
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_stream = TwitterStream(auth=oauth)
+tweet_stream_source = twitter_stream.statuses.sample()
+
+class Producer(object):
+
+    def __init__(self, dns):
+        self.producer = KafkaProducer(bootstrap_servers = 'localhost:9092')
+    def produce_msgs(self,source):
+	for drop in source:
+        	print json.dumps(drop)
+		self.producer.send('my-topic',json.dumps(drop))
+		time.sleep(1)
+
+if __name__ == "__main__":
+    args = sys.argv
+    dns = str(args[1])
+    prod = Producer(dns)
+    prod.produce_msgs(tweet_stream_source)
