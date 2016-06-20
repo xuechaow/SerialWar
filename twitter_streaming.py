ec675c445b681bd302524d4f3bc693eaf483f9db
Ubuntu <ubuntu@ip-172-31-2-104.us-west-2.compute.internal>
2016-06-20 19:00:52 +0000

    adding avro serialization part

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -15,18 +15,21 @@ oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter_stream = TwitterStream(auth=oauth)
 tweet_stream_source = twitter_stream.statuses.sample()
 
-class Producer(object):
+class Producer():
 
-    def __init__(self, dns):
+    def __init__(self):
         self.producer = KafkaProducer(bootstrap_servers = 'localhost:9092')
     def produce_msgs(self,source):
 	for drop in source:
-        	print json.dumps(drop)
-		self.producer.send('my-topic',json.dumps(drop))
-		time.sleep(1)
+        	if 'text' in drop:
+			message = json.dumps(drop)
+			self.producer.send('my-topic',message)
+			print(message)
+			self.producer.send('message-size',len(message))
+	#	time.sleep(1)
 
 if __name__ == "__main__":
     args = sys.argv
-    dns = str(args[1])
-    prod = Producer(dns)
+    #dns = str(args[1])
+    prod = Producer()
     prod.produce_msgs(tweet_stream_source)
