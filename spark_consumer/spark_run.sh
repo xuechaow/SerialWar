f1deeb088959468945aca26f1c22379f6c4fa8f9
Ubuntu <ubuntu@ip-172-31-2-108.us-west-2.compute.internal>
2016-06-21 06:45:27 +0000

    updated json consumer

--- spark_consumer/spark_run.sh
+++ spark_consumer/spark_run.sh
@@ -1,1 +1,1 @@
-spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.1 spark_kafka_consumer.py
+spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.1 spark_kafka_consumer.py Raw-JSON 1
